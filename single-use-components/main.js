91dcb584045afb4af6e80e975d4e19448053b137
CJEdgerton <cjedgerton@gmail.com>
2017-02-20 21:07:39 -0500

    Single use components, and vue component files.

---
+++ single-use-components/main.js
@@ -0,0 +1,13 @@
+Vue.component('progress-view', {
+	data() {
+		return {
+			 completionRate: 50
+		};
+	}
+});
+app = new Vue({
+	el: '#root',
+	data: {
+		//	
+	}
+});
---
+++ webpack-cli/hello/src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="box">
+		<slot></slot>	
+	</div>	
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+
+	.box {
+		background: gray;
+		padding: 10px;
+		margin-bottom: 10px;
+		border: 1px solid red;
+	}
+
+</style>
