dce40516bdfbefe17ba2a2469c996d147016c610
CJEdgerton <cjedgerton@gmail.com>
2017-02-21 21:48:49 -0500

    Hot module replacement.

---
+++ hot-module-replacement/lesson/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<h2>The count is {{ count }}</h2>
+		<p>
+			<button @click="count++">Increment Count</button>
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				count: 0
+			};
+		}
+	}
+</script>
