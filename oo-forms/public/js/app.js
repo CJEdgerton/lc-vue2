810c1e343a8beb88a1846db074afcbbe1228a8cd
CJEdgerton <cjedgerton@gmail.com>
2017-03-03 12:12:13 -0500

    OO forms part 3

--- oo-forms/public/js/app.js
+++ oo-forms/public/js/app.js
@@ -1,14 +1,18 @@
 class Errors {
+
 	constructor() {
 		this.errors = {}; 
 	}
+
 	get(field) {
 		if( this.errors[field])
 			return this.errors[field][0];
 	}
+
 	record(errors) {
 		this.errors = errors;
 	}
+
 	clear(field) {
 		if(field) {
 			delete this.errors[field];
@@ -17,15 +21,19 @@ class Errors {
 
 		this.errors = {};
 	}
+
 	has(field) {
 		return this.errors.hasOwnProperty(field);
 	}
+
 	any() {
 		return Object.keys(this.errors).length > 0;
 	}
+
 }
 
 class Form {
+
 	constructor(data) {
 		this.originalData = data;
 
@@ -40,33 +48,48 @@ class Form {
 		for( let field in this.originalData) {
 			this[field] = '';
 		}
+		this.errors.clear();
 	}
 
 	data() {
-		let data = Object.assign({}, this);
 
-		delete data.originalData;
-		delete data.errors;
+		let data = {};
+
+		for ( let property in this.originalData ) {
+			data[property] = this[property];
+		}
 
 		return data;
 	}
 
 	submit(requestType, url) {
-		axios[requestType]( url,  this.data() )
-			.then( this.onSuccess.bind(this) )
-			.catch( this.onFail.bind(this));
+		return new Promise((resolve, reject) => {
+
+			axios[requestType]( url,  this.data() )
+				.then( response => {
+					this.onSuccess(response.data);
+
+					resolve(response.data);
+				})
+				.catch( error => {
+					this.onFail(error.response.data);
+
+					reject(error.response.data);
+				});
+
+		});
 	}
 	
-	onSuccess(response) {
-		alert(response.data.message);
+	onSuccess(data) {
+		alert(data.message);
 
-		this.errors.clear();
 		this.reset();
 	}	
 
-	onFail(error) {
-		this.errors.record( error.response.data );
+	onFail(errors) {
+		this.errors.record( errors );
 	}
+
 }
 
 new Vue({
@@ -81,7 +104,9 @@ new Vue({
 
 	methods: {
 		onSubmit() {
-			this.form.submit('post', '/projects');
+			this.form.submit('post', '/projects')
+				.then(data => alert(data))
+				.catch(errors => console.log(errors));
 		}
 	}
 
